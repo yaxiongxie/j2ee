27aaf63a068081be054e6fcca047a0202b784cc4
Administrator <Administrator@PC-20160605RUYG>
2016-06-16 15:58:03 +0800

    platform

---
+++ TestWeb/WebRoot/modules/index/js/myApp.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2016/6/16.
+ */
+
+(function (ng) {
+    'use strict';
+    var app = ng.module('ngLoadScript', []);
+    app.directive('script', function() {
+        return {
+            restrict: 'E',
+            scope: false,
+            link: function(scope, elem, attr)
+            {
+                if (attr.type==='text/javascript-lazy')
+                {
+                    var s = document.createElement("script");
+                    s.type = "text/javascript";
+                    var src = elem.attr('src');
+                    if(src!==undefined)
+                    {
+                        s.src = src;
+                    }
+                    else
+                    {
+                        var code = elem.text();
+                        s.text = code;
+                    }
+                    document.head.appendChild(s);
+                    elem.remove();
+                }
+            }
+        };
+    });
+}(angular));
+
+var myApp=angular.module('myApp',['ngLoadScript','ui.router']);
